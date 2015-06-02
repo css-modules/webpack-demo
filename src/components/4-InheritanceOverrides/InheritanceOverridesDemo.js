8d1c4df47d6347a5550f1bbd2c76f641cf24832b
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2015-06-02 21:23:22 +1000

    Expand inheritance overrides demo

--- src/components/4-InheritanceOverrides/InheritanceOverridesDemo.js
+++ src/components/4-InheritanceOverrides/InheritanceOverridesDemo.js
@@ -4,6 +4,8 @@ import React, { Component } from 'react';
 
 import js from '!!raw!./InheritanceOverrides.js';
 import css from '!!raw!./InheritanceOverrides.css';
+import layoutCss from '!!raw!shared/styles/layout.css';
+import typographyCss from '!!raw!shared/styles/typography.css';
 import Snippet from 'shared/Snippet/Snippet';
 
 export default class InheritanceOverridesDemo extends Component {
@@ -11,7 +13,9 @@ export default class InheritanceOverridesDemo extends Component {
   render() {
     const files = [
       { name: 'InheritanceOverrides.js', source: js },
-      { name: 'InheritanceOverrides.css', source: css }
+      { name: 'InheritanceOverrides.css', source: css },
+      { name: 'shared/styles/layout.css', source: layoutCss },
+      { name: 'shared/styles/typography.css', source: typographyCss }
     ];
 
     return (
