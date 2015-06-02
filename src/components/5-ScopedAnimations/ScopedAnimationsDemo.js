cdc2467e905aa4c260c9ce451ec9aeb74f31f0ae
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2015-06-02 21:29:35 +1000

    Expand scoped animations demo

--- src/components/5-ScopedAnimations/ScopedAnimationsDemo.js
+++ src/components/5-ScopedAnimations/ScopedAnimationsDemo.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 
 import js from '!!raw!./ScopedAnimations.js';
 import css from '!!raw!./ScopedAnimations.css';
+import animationsCss from '!!raw!shared/styles/animations.css';
 import Snippet from 'shared/Snippet/Snippet';
 
 export default class ScopedAnimationsDemo extends Component {
@@ -11,7 +12,8 @@ export default class ScopedAnimationsDemo extends Component {
   render() {
     const files = [
       { name: 'ScopedAnimations.js', source: js },
-      { name: 'ScopedAnimations.css', source: css }
+      { name: 'ScopedAnimations.css', source: css },
+      { name: 'shared/styles/animations.css', source: animationsCss }
     ];
 
     return (
