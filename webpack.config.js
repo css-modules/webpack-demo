92b0236beb6d371cbfa9dd12e111df430a4bb063
Etty Sekhon <sekhoe01@118-UX.local>
2016-01-22 01:02:53 +0000

    add url loader back

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') },
-      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, loader : 'file-loader?name=fonts/[name].[ext]' }
+      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, loader : 'url-loader?limit=10000&name=fonts/[name].[ext]' }
     ]
   },
 
